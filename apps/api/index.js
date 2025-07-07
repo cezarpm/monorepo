ef8a97357fcd2360da09e4b396cc00777407390e
cezarpm <cezar_pm@hotmail.com>
2025-07-07 17:16:41 -0300

    feat: api

---
+++ apps/api/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const books = [
+  { id: 1, title: "O Hobbit", author: "J.R.R. Tolkien" },
+  { id: 2, title: "1984", author: "George Orwell" },
+  { id: 3, title: "Dom Casmurro", author: "Machado de Assis" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express 🚀");
+});
+
+// Rota GET /books
+app.get("/books", (_req, res) => {
+  res.json(books);
+});
+
+app.listen(PORT, () => {
+  console.log(`API running at http://localhost:${PORT}`);
+});
