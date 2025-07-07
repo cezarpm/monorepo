25394192f0f4c38d1f168dda58afa6ec63b1f3ac
cezarpm <cezar_pm@hotmail.com>
2025-07-07 16:00:11 -0300

    fix

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,30 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: {
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    exclude: ["node_modules"],
-  },
-  assetsInclude: ["**/*.xlsx"],
-  define: {
-    global: "globalThis",
-  },
   plugins: [react()],
-  server: {
-    host: "localhost",
-    port: 3000,
-  },
-  preview: {
-    host: "localhost",
-    port: 3000,
-  },
 });
