4b1f19c3fb76052fcaa21efa5ce1953efd0c9f4a
cezarpm <cezar_pm@hotmail.com>
2025-07-07 16:59:35 -0300

    feat: ui lib changes

--- packages/ui/components/header.tsx
+++ packages/ui/components/header.tsx
@@ -6,7 +6,15 @@ interface HeaderProps {
 
 export const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
-    <header id="header" style={{ color: "red" }}>
+    <header
+      id="header"
+      style={{
+        color: "#f1f1f1",
+        fontSize: 30,
+        fontFamily: "Montserrat",
+        backgroundColor: "purple",
+      }}
+    >
       <h1>{title}</h1>
     </header>
   );
