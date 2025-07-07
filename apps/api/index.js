a15226abc0a039992469004e0100505bfe60ea98
cezarpm <cezar_pm@hotmail.com>
2025-07-07 17:55:52 -0300

    feat: add new book

--- apps/api/index.js
+++ apps/api/index.js
@@ -9,6 +9,7 @@ const books = [
   { id: 1, title: "O Hobbit", author: "J.R.R. Tolkien" },
   { id: 2, title: "1984", author: "George Orwell" },
   { id: 3, title: "Dom Casmurro", author: "Machado de Assis" },
+  { id: 4, title: "Senhor dos anéis", author: "J.R.R. Tolkien" },
 ];
 
 app.get("/", (req, res) => {
