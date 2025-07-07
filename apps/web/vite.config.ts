3e04f80c543b4127c952456eae251ff303d97891
cezarpm <cezar_pm@hotmail.com>
2025-07-07 15:29:04 -0300

    change vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,5 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "lodash"], // Exemplo de como agrupar dependências comuns
+        },
+      },
+    },
+  },
+  optimizeDeps: {
+    exclude: ["node_modules"],
+  },
+  assetsInclude: ["**/*.xlsx"],
+  define: {
+    global: "globalThis",
+  },
   plugins: [react()],
+  server: {
+    host: "localhost",
+    port: 3000,
+  },
+  preview: {
+    host: "localhost",
+    port: 3000,
+  },
 });
